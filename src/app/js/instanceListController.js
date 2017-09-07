cd244e2d16de7e291f26e9300d2ecc4d79aa4208
qqfbleach <qianqifeng@hotmail.com>
2017-09-08 00:37:15 +0800

    1.增加表格进度条及操作按钮 2.删除css、html多余文件

--- src/app/js/instanceListController.js
+++ src/app/js/instanceListController.js
@@ -12,4 +12,22 @@ app.controller("instanceCtrl", function ($scope, $http, $state) {
 
     );
 
+    $scope.instanceWidthStyle = {
+        "width": "40%"
+    };
+
+    var data = [];
+    for (var i = 0; i < 5; i++) {
+        var tmp = {};
+        tmp.name = "qqf";
+        tmp.description = "instance1";
+        tmp.node = "node1";
+        tmp.progress = { "width": "40%" };
+        tmp.progressText = "40%";
+        data.push(tmp);
+    }
+
+
+    $scope.instances = data;
+
 });
