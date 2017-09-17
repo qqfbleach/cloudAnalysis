396304c5d0c3de1a5009b5cd7bbc9fb05af8fad8
somewhat6139 <swh6139@163.com>
2017-09-17 17:27:02 +0800

    run.py access parameters(posifile,negfile,predictfile,percent). 4 parameters use default value if not assigned

--- src/dataAnalysis/test/run.py
+++ src/dataAnalysis/test/run.py
@@ -3,6 +3,19 @@
 
 import sys
 sys.path.append("..")
-from algorithms.svm_nonlinear import plot_svm_nonlinear
+#from algorithms.svm_nonlinear import plot_svm_nonlinear
+from algorithms.svm import svm
 
-plot_svm_nonlinear.plot()
+if __name__ == "__main__":
+    len = len(sys.argv)
+    if len >= 2:
+        posi = sys.argv[1]
+    if len >= 3:
+        neg = sys.argv[2]
+    if len >= 4:
+        predict = sys.argv[3]
+    if len >= 5:
+        percent = sys.argv[4]
+    #plot_svm_nonlinear.plot()
+    #svm.dosvm(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+    svm.dosvm()
