85e07f1c2cf48c0493a5e748710cb936691d55ba
somewhat6139 <swh6139@163.com>
2017-09-17 20:21:36 +0800

    Fix TypeError when call httpsConn.request function

--- src/dataAnalysis/algorithms/utils/monitor.py
+++ src/dataAnalysis/algorithms/utils/monitor.py
@@ -7,19 +7,33 @@ from algorithms.utils import fileop
 import json
 
 https = False
-url = "/cloud/v1/instance/status/:"
 serverinfo = "/home/server_info"
        
 def gethost():
     if fileop.pathexist(serverinfo) == True:
         fd = open(serverinfo)
         setting = json.load(fd)
-        host = setting['host']
+        hostraw = setting['host']
+        index = hostraw.find('/')
+        host = hostraw[0:index]
         fd.close()
     else:
         print "server_info file:" + serverinfo + " is not exist!"
         host = -1
     return host
+
+def geturl():
+    if fileop.pathexist(serverinfo) == True:
+        fd = open(serverinfo)
+        setting = json.load(fd)
+        hostraw = setting['host']
+        index = hostraw.find('/')
+        url = hostraw[index:len(hostraw)]
+        fd.close()
+    else:
+        print "server_info file:" + serverinfo + " is not exist!"
+        url = -1
+    return url
 
 def getnodeid():
     if fileop.pathexist(serverinfo) == True:
@@ -33,24 +47,21 @@ def getnodeid():
     return nodeid
 
 def sendstatus(process,msg,ishttps):
-    global https, host, url
+    host = gethost()
     if ishttps == True:
-        httpsConn = httplib.HTTPSConnection(gethost())
+        httpsConn = httplib.HTTPSConnection(host)
         sock = socket.create_connection((httpsConn.host, httpsConn.port))
         httpsConn.sock = ssl.wrap_socket(sock, ca_certs=CERT_FILE,cert_reqs=ssl.CERT_REQUIRED, ssl_version=ssl.PROTOCOL_SSLv3)
     else:
-        httpsConn = httplib.HTTPConnection(gethost())
+        httpsConn = httplib.HTTPConnection(host)
     body = {
             "task_id":3344,
             "task_result":"",
             "process":process,
             "status":msg
-            }
-    if getnodeid() == -1:
-        print "Id error!"
-    else:
-        url = url + str(getnodeid())
-    httpsConn.request("POST", url, body)
+           }
+    url = geturl()
+    httpsConn.request("POST", url, json.JSONEncoder().encode(body))
     res = httpsConn.getresponse()
     print res.status, res.reason
     if httpsConn:
@@ -59,4 +70,4 @@ def sendstatus(process,msg,ishttps):
 def sendcurstatus(process,msg):
     #msg = msg + " in function " + sys._getframe().f_back.f_code.co_name
     #print msg
-    sendstatus(process, msg, https)
+    sendstatus(process, msg, False)
