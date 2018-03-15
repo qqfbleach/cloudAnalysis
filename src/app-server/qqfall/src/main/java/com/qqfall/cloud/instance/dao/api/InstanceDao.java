a367264c9ad5bdb3b966f87bc1a8ee6a06f3c6d5
qianqifeng@hotmail.com <qqf67167521>
2018-03-15 22:15:56 +0800

    代码重构。使用spring boot作为后台框架，spring security作为鉴权，UI增加登陆页面。 TODO：后台api

---
+++ src/app-server/qqfall/src/main/java/com/qqfall/cloud/instance/dao/api/InstanceDao.java
@@ -0,0 +1,10 @@
+package com.qqfall.cloud.instance.dao.api;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.qqfall.cloud.instance.dao.entity.InstanceEntity;
+
+
+public interface InstanceDao extends MongoRepository<InstanceEntity, Long>,InstanceDaoCustom{
+
+}
